6bd7009bd5ecf6310cbd659d280739fb6c333862
hansmoen <hnsmoen@gmail.com>
2026-08-11 23:28:16 +0200

    Added Google Colab compatibility

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,13 +1,19 @@
 from dotenv import dotenv_values
 import os
+import sys
 from pathlib import Path
 
 
-
 def get_api_key():
-    project_root = Path(__file__).resolve().parents[2]
-    secrets_file = os.path.join(project_root, "secrets", "keys.env")
-    secrets = dotenv_values(secrets_file)
-    api_key = secrets["OPENAI_API_KEY"]
-    return api_key
+    IN_COLAB = "google.colab" in sys.modules
+    if IN_COLAB:
+        from google.colab import userdata
+        api_key = userdata.get('api_key_1')
+        return api_key
+    else:
+        project_root = Path(__file__).resolve().parents[2]
+        secrets_file = os.path.join(project_root, "secrets", "keys.env")
+        secrets = dotenv_values(secrets_file)
+        api_key = secrets["OPENAI_API_KEY"]
+        return api_key
 
