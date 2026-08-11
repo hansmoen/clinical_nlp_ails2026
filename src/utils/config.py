7511302cc09223eb189fb019e8bfaec6f7050d38
hansmoen <hnsmoen@gmail.com>
2026-08-11 23:29:57 +0200

    Added Google Colab compatibility

--- src/utils/config.py
+++ src/utils/config.py
@@ -4,6 +4,13 @@ import sys
 from pathlib import Path
 
 
+"""
+NB! Regarding API keys.
+If you are using Google Colab, open 'Secrets' and add a key with 'Name': "MY_API_KEY" and 'Value': {your key}.
+Also make sure to also enable "Notebook access".
+Otherwise, add a folder on root level named 'secrets', create a file inside called keys.env, then add to it: "OPENAI_API_KEY={your key}".
+"""
+
 def get_api_key():
     IN_COLAB = "google.colab" in sys.modules
     if IN_COLAB:
